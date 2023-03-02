5fbb150ff3d35035c2b7c434778263c36d55db5c
Jasurbeek17 <jasurismatov50@gmail.com>
2023-03-02 17:47:22 +0500

    Code was done refactor

--- Program.cs
+++ Program.cs
@@ -1,62 +1,27 @@
-﻿//Kescha
-//28.02.2023
+﻿//Kescha OOP
+//02.03.2023
 
-//name
-System.Console.Write("\nEnter your name: ");
-string name = Console.ReadLine();
-string greeting = $"Hello, {name}. My name is 'Kescha'\n";
-System.Console.WriteLine(greeting);
+using Kescha.Classes;
 
-//age
-System.Console.Write("Enter your age: ");
-int age = Convert.ToInt32(Console.ReadLine());
-int keschaAge = 3;
-System.Console.WriteLine($"Wondurful! Your age is {age} and my age is {keschaAge} years old.\n");
-
-//ageDifference
-int ageDifference = Math.Abs(age - keschaAge);
-System.Console.WriteLine($"Our ages defference is {ageDifference}.");
-
-//who is big
-if (ageDifference > 0)
+internal class Program
 {
-    System.Console.WriteLine("You are than me older!\n");
-}
-else if (ageDifference < 0 ) 
-{
-    System.Console.WriteLine("You are than me younger!\n");
-}
-else 
-{
-    System.Console.WriteLine("We are the same age!\n");
-}
-
-// friends y/n
-char doFriendsTell = ' ';
-System.Console.Write("Do you tell about my friends? (y/n): ");
-doFriendsTell = Console.ReadLine()[0];
-if (doFriendsTell.Equals('y')) {
-
-    //Kescha's Friends
-    string[] keschaFriends = {"Kasya", "Vasya", "Valya", name};
-    int[] friendsAges = new int [4];
-    friendsAges[0] = 3;
-    friendsAges[1] = 4;
-    friendsAges[2] = 4;
-    friendsAges[3] = age;
-
-    for (int i = 0; i < friendsAges.Length; i ++ ) 
+    private static void Main(string[] args)
     {
-        System.Console.WriteLine($"This is my friend {keschaFriends[i]}. He is {friendsAges[i]} years old.");
-    }
-    System.Console.WriteLine();
+        Animal kescha = new Animal(name: "Kescha", age: 3);
+        
+        //name
+        Console.Write("\nEnter your name: ");
+        string userName = Console.ReadLine();
 
-}
-else if (doFriendsTell.Equals('n')) 
-{
-    System.Console.WriteLine("Okey.\n");
-}
-else 
-{
-    System.Console.WriteLine("You enter other sign! \n");
+        //age
+        Console.Write("Enter your age: ");
+        int userAge = Convert.ToInt32(Console.ReadLine());
+
+        kescha.Greets(userName);
+        kescha.CalculateAgeDifferenceWithNoReturn(userAge);
+        kescha.AgesAboutMessagePrint(userAge, animalAge: kescha.Age);
+        kescha.AgesDifferenceAboutPrint();
+        kescha.CompareAges();
+        kescha.TellAboutFriends(userName, userAge);
+    }
 }
